b89cc46ae203d2ddc2e1c6dce32b30e324da241c
Maxime Diop <adou@MacBook-Pro-de-Maxime.local>
2019-08-28 20:07:45 +0200

    responsive tablette

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,11 +1,12 @@
-const $page = $('.page');
+const $page = $(".page");
+const $square = $(".square");
 
-$('.menu_toggle').on('click', function() {
-  $page.toggleClass('shazam');
+$(".menu_toggle").on("click", function() {
+  $page.toggleClass("shazam");
 });
-$('.content').on('click', function() {
-  $page.removeClass('shazam');
+$(".content").on("click", function() {
+  $page.removeClass("shazam");
 });
-$('.menu_items').on('click', function() {
-  $page.removeClass('shazam');
+$(".menu_items").on("click", function() {
+  $page.removeClass("shazam");
 });
